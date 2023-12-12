f6d91854f0aead28b45651b6265b4d8ddb38ed95
Rachapro <phacharapalawong3@gmail.com>
2023-12-12 13:35:03 +0700

    https://github.com/ne0natalz/lab7

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,18 +2,12 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
-}
-
-int main(){
-	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
-	cout << before('P') << "\n";
-	cout << before('T') << "\n";
-	cout << before('Z') << "\n";
-	cout << before('a') << "\n";
-	cout << before('0') << "\n";
-	cout << before('c') << "\n";
-	return 0;
-}
+	if (x == 'A') {
+        return 'Z';
+    } else if (x >= 'B' && x <= 'Z') {
+        return x - 1;
+    } else {
+        // Handle invalid x (not 'A' to 'Z')
+        return '0';
+    }
+    }
